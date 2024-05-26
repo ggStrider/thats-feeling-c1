70b8f128d784ce854e9958d2c4efd2654b32192d
Yaroslav Oslam <ggstrider1@gmail.com>
2024-05-26 21:28:11 +0300

    refactored, updated initialize inventory system

--- Assets/DataModel/InitializeItems.cs
+++ Assets/DataModel/InitializeItems.cs
@@ -1,49 +1,54 @@
 using UnityEngine;
-using UnityEngine.UI;
 
-using TMPro;
-
-using System;
 using System.Collections.Generic;
+using Items;
+using UI;
 
 namespace DataModel
 {
     public class InitializeItems : MonoBehaviour
     {
-        [SerializeField] private List<ItemSlot> _itemSlot;
+        [SerializeField] private List<InventorySlotShowInfo> _slots;
         
         private GameSession _session;
+        
         private void Start()
         {
             _session = GetComponent<GameSession>();
             _Initialize();
         }
 
+        public void _AddToUI(ItemInfo item)
+        {
+            var slot = FindFreeSlot();
+
+            if (slot == null)
+            {
+                Debug.Log("No free slots! / Didnt add slots to array");
+                return;
+            }
+
+            slot.CurrentItem = item;
+            slot.ReDrawItemInInventory();
+        }
+
+        private InventorySlotShowInfo FindFreeSlot()
+        {
+            foreach (var slot in _slots)
+            {
+                if (slot.CurrentItem != null) continue;
+                return slot;
+            }
+
+            return null;
+        }
+        
         [ContextMenu("Initialize")]
         public void _Initialize()
         {
             var items = _session.Data.Items;
 
             if (items.Count == 0) return;
-            
-            for (var i = 0; i < items.Count; i++)
-            {
-                _itemSlot[i].Text.text = items[i].Name;
-                _itemSlot[i].ImageComponent.sprite = items[i].ItemImage;
-            }
-        }
-
-        [Serializable]
-        public class ItemSlot
-        {
-            public TextMeshProUGUI Text;
-            public Image ImageComponent;
-
-            public ItemSlot(TextMeshProUGUI text, Image image)
-            {
-                Text = text;
-                ImageComponent = image;
-            }
         }
     }
 }
