edf8f6bbb67461f1276ab25da0c0976f0642245c
Yaroslav Oslam <ggstrider1@gmail.com>
2024-05-26 21:40:33 +0300

    Added system which toggle animation for inventory

---
+++ Assets/Scripts/Player/CheckInventory.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+namespace Player
+{
+    public class CheckInventory : MonoBehaviour
+    {
+        [SerializeField] private Animator _animator;
+        [SerializeField] private bool _isChecking;
+        
+        private static readonly int IsCheckingKey = Animator.StringToHash("is-checking");
+
+        public void _ToggleCheckAnimation()
+        {
+            _isChecking = !_isChecking;
+            _animator.SetBool(IsCheckingKey, _isChecking);
+
+            if (_isChecking)
+            {
+                Cursor.visible = true;
+                Cursor.lockState = CursorLockMode.None;
+            }
+            else
+            {
+                Cursor.visible = false;
+                Cursor.lockState = CursorLockMode.Locked;
+            }
+        }
+    }
+}
